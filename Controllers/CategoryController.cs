305de204a7ef84389472ffc8502f64d59a90df8c
Always-Learningeek <myrealemail1400@gmail.com>
2025-02-23 23:09:29 +0100

    Category added to our SQL_Database
    also some bug fixed

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMarket.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace WebMarket.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMarket.Web.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime CreateDateTime { get; set; }
