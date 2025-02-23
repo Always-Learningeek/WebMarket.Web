f020f1971536961759063bbacfe847c6890dbbc0
Always-Learningeek <myrealemail1400@gmail.com>
2025-02-23 20:01:49 +0100

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebMarket.Web.Models;
+
+namespace WebMarket.Web.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace WebMarket.Web.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; }
+
+
+    }
+}
